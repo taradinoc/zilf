182dc18d458e09d2ce648fa3bbcc27b52d7b6157
Jesse McGrew <4934394+taradinoc@users.noreply.github.com>
2012-11-12 04:18:53 +0000

    Added makedist script and dist input files. Added order-of-evaluation test. Updated ignore lists.

---
+++ Testing/order.zil
@@ -0,0 +1,6 @@
+<ROUTINE GO () <MYGO>>
+
+<ROUTINE MYGO () <PRINTN <+ <FOO> <BAR>>>>
+
+<ROUTINE FOO () <PRINTI "foo!"> <CRLF> 3>
+<ROUTINE BAR () <PRINTI "bar"> <CRLF> 4>
---
+++ makedist.bat
@@ -0,0 +1,33 @@
+@echo off
+if not exist zilf\bin\debug\zilf.exe goto missing
+if not exist zapf\bin\debug\zapf.exe goto missing
+
+rd /s /q dist
+mkdir dist
+mkdir dist\bin
+mkdir dist\doc
+mkdir dist\library
+mkdir dist\sample
+
+copy distfiles\README.txt dist
+
+copy zilf\bin\debug\Antlr3.Runtime.dll dist\bin
+copy distfiles\AntlrLicense.txt dist\bin
+copy zilf\bin\debug\zilf.exe dist\bin
+copy zilf\COPYING.txt dist\bin
+copy zapf\bin\debug\zapf.exe dist\bin
+
+copy zilf_manual.html dist\doc
+copy zapf_manual.html dist\doc
+
+copy library\*.zil dist\library
+copy library\LICENSE.txt dist\library
+copy library\ZIL_ZILF_differences.txt dist\library
+
+copy examples\*.zil dist\sample
+
+exit /b 0
+
+:missing
+echo Can't find compiled program files.
+exit /b 1
