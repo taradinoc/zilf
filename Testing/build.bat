5efe9d6334a39f1d30b1fcfc83a932369608b0ef
Jesse McGrew <jmcgrew@hansprestige.com>
2014-04-07 01:11:54 -0700

    build.bat: Copy Zilf.Emit.dll

--- Testing/build.bat
+++ Testing/build.bat
@@ -1,5 +1,6 @@
 @echo off
 xcopy /d /y ..\zilf\bin\debug\zilf.exe .
+xcopy /d /y ..\zilf\bin\debug\Zilf.Emit.dll .
 xcopy /d /y ..\zilf\bin\debug\Antlr3.Runtime.dll .
 xcopy /d /y ..\zapf\bin\debug\zapf.exe .
 xcopy /d /y ..\library\parser.zil .
