4d6c660ca4748e5e81cdcc86e0edf3a2ba680ca5
Jesse McGrew <4934394+taradinoc@users.noreply.github.com>
2014-05-23 21:10:12 -0700

    Split debug file writing code into a separate class, BinaryDebugFileWriter.

---
+++ Zapf/BinaryDebugFileWriter.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Zapf
+{
+    class BinaryDebugFileWriter : IDebugFileWriter
+    {
+        private readonly Stream stream;
+        private ushort nextRoutineNumber;
+        private long routineStart = -1;
+        private int routinePoints = -1;
+
+        public BinaryDebugFileWriter(Stream debugStream)
+        {
+            this.stream = debugStream;
+
+            WriteDebugWord(0xDEBF);     // magic number
+            WriteDebugWord(0);          // file format
+            WriteDebugWord(2001);       // creator version
+        }
+
+        public bool InRoutine
+        {
+            get { return routinePoints >= 0; }
+        }
+
+        public void Close()
+        {
+            stream.WriteByte(DEBF.EOF_DBR);
+            stream.Close();
+        }
+
+        public void WriteMap(IEnumerable<KeyValuePair<string, int>> map)
+        {
+            WriteDebugByte(DEBF.MAP_DBR);
+            foreach (var pair in map)
+            {
+                WriteDebugString(pair.Key);
+                WriteDebugAddress(pair.Value);
+            }
+            WriteDebugByte(0);
+        }
+
+        public void WriteHeader(byte[] header)
+        {
+            WriteDebugByte(DEBF.HEADER_DBR);
+            stream.Write(header, 0, 64);
+        }
+
+        public void WriteAction(ushort value, string name)
+        {
+            WriteDebugByte(DEBF.ACTION_DBR);
+            WriteDebugWord(value);
+            WriteDebugString(name);
+        }
+
+        public void WriteArray(ushort offsetFromGlobal, string name)
+        {
+            WriteDebugByte(DEBF.ARRAY_DBR);
+            WriteDebugWord(offsetFromGlobal);
+            WriteDebugString(name);
+        }
+        
+        public void WriteAttr(ushort value, string name)
+        {
+            WriteDebugByte(DEBF.ATTR_DBR);
+            WriteDebugWord(value);
+            WriteDebugString(name);
+        }
+
+        public void WriteClass(string name, LineRef start, LineRef end)
+        {
+            WriteDebugByte(DEBF.CLASS_DBR);
+            WriteDebugString(name);
+            WriteDebugLineRef(start);
+            WriteDebugLineRef(end);
+        }
+
+        public void WriteFakeAction(ushort value, string name)
+        {
+            WriteDebugByte(DEBF.FAKE_ACTION_DBR);
+            WriteDebugWord(value);
+            WriteDebugString(name);
+        }
+
+        public void WriteFile(byte number, string includeName, string actualName)
+        {
+            WriteDebugByte(DEBF.FILE_DBR);
+            WriteDebugByte(number);
+            WriteDebugString(includeName);
+            WriteDebugString(actualName);
+        }
+
+        public void WriteGlobal(byte number, string name)
+        {
+            WriteDebugByte(DEBF.GLOBAL_DBR);
+            WriteDebugByte(number);
+            WriteDebugString(name);
+        }
+
+        public void WriteLine(LineRef loc, int address)
+        {
+            if (!InRoutine)
+                throw new InvalidOperationException("WriteLine must be called inside a routine");
+
+            WriteDebugLineRef(loc);
+            WriteDebugWord((ushort)(address - routineStart));
+            routinePoints++;
+        }
+
+        public void WriteObject(ushort number, string name, LineRef start, LineRef end)
+        {
+            WriteDebugByte(DEBF.OBJECT_DBR);
+            WriteDebugWord(number);
+            WriteDebugString(name);
+            WriteDebugLineRef(start);
+            WriteDebugLineRef(end);
+        }
+
+        public void WriteProp(ushort number, string name)
+        {
+            WriteDebugByte(DEBF.PROP_DBR);
+            WriteDebugWord(number);
+            WriteDebugString(name);
+        }
+
+        public void StartRoutine(LineRef start, int address, string name, IEnumerable<string> locals)
+        {
+            WriteDebugByte(DEBF.ROUTINE_DBR);
+            WriteDebugWord(nextRoutineNumber);
+            WriteDebugLineRef(start);
+            WriteDebugAddress(address);
+            WriteDebugString(name);
+            foreach (var local in locals)
+                WriteDebugString(local);
+            WriteDebugByte(0);
+
+            // start LINEREF_DBR block
+            WriteDebugByte(DEBF.LINEREF_DBR);
+            WriteDebugWord(nextRoutineNumber);
+            WriteDebugWord(0);      // # sequence points, filled in later
+
+            routinePoints = 0;
+            routineStart = address;
+        }
+
+        public void EndRoutine(LineRef end, int address)
+        {
+            // finish LINEREF_DBR block
+            var curPosition = stream.Position;
+            stream.Position -= 2 + routinePoints * 6;
+            WriteDebugWord((ushort)routinePoints);
+            stream.Position = curPosition;
+            routinePoints = -1;
+            routineStart = -1;
+
+            // write ROUTINE_END_DBR block
+            WriteDebugByte(DEBF.ROUTINE_END_DBR);
+            WriteDebugWord(nextRoutineNumber++);
+            WriteDebugLineRef(end);
+            WriteDebugAddress(address);
+        }
+
+        #region Write Primitives
+
+        private void WriteDebugByte(byte b)
+        {
+            stream.WriteByte(b);
+        }
+
+        private void WriteDebugWord(ushort w)
+        {
+            stream.WriteByte((byte)(w >> 8));
+            stream.WriteByte((byte)w);
+        }
+
+        private void WriteDebugAddress(int a)
+        {
+            stream.WriteByte((byte)(a >> 16));
+            stream.WriteByte((byte)(a >> 8));
+            stream.WriteByte((byte)a);
+        }
+
+        private void WriteDebugLineRef(LineRef lineRef)
+        {
+            stream.WriteByte(lineRef.File);
+            stream.WriteByte((byte)(lineRef.Line >> 8));
+            stream.WriteByte((byte)lineRef.Line);
+            stream.WriteByte(lineRef.Col);
+        }
+
+        private void WriteDebugString(string s)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(s);
+            stream.Write(bytes, 0, bytes.Length);
+            stream.WriteByte(0);
+        }
+
+        #endregion
+    }
+}
---
+++ Zapf/IDebugFileWriter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zapf
+{
+    struct LineRef
+    {
+        public LineRef(byte file, ushort line, byte col)
+        {
+            this.File = file;
+            this.Line = line;
+            this.Col = col;
+        }
+
+        public readonly byte File;
+        public readonly ushort Line;
+        public readonly byte Col;
+
+        public static bool operator ==(LineRef a, LineRef b)
+        {
+            return a.File == b.File &&
+                a.Line == b.Line &&
+                a.Col == b.Col;
+        }
+
+        public static bool operator !=(LineRef a, LineRef b)
+        {
+            return a.File != b.File ||
+                a.Line != b.Line ||
+                a.Col != b.Col;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is LineRef && ((LineRef)obj) == this;
+        }
+
+        public override int GetHashCode()
+        {
+            int result = File;
+            result = result * 31 + Line;
+            result = result * 31 + Col;
+            return result;
+        }
+    }
+
+    interface IDebugFileWriter
+    {
+        void Close();
+
+        void StartRoutine(LineRef start, int address, string name, IEnumerable<string> locals);
+        bool InRoutine { get; }
+        void WriteLine(LineRef loc, int address);
+        void EndRoutine(LineRef end, int address);
+
+        void WriteAction(ushort number, string name);
+        void WriteArray(ushort offsetFromGlobal, string name);
+        void WriteAttr(ushort number, string name);
+        void WriteClass(string name, LineRef start, LineRef end);
+        void WriteFakeAction(ushort number, string name);
+        void WriteFile(byte number, string includeName, string actualName);
+        void WriteGlobal(byte number, string name);
+        void WriteHeader(byte[] header);
+        void WriteMap(IEnumerable<KeyValuePair<string, int>> map);
+        void WriteObject(ushort number, string name, LineRef start, LineRef end);
+        void WriteProp(ushort number, string name);
+    }
+}
