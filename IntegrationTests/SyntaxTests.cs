e496e1ce1d9f23c4d0f023b5ce59e3d60bfd76e1
Jesse McGrew <4934394+taradinoc@users.noreply.github.com>
2015-04-05 11:40:11 -0700

    Allow syntax lines to set an action number independent of the action routine: <SYNTAX FOO OBJECT = V-FOO>  ;"PRSA = V?FOO" <SYNTAX FOO OBJECT WITH OBJECT = V-FOO <> FOO-WITH>  ;"PRSA = V?FOO-WITH"
    Preactions are linked to the action number, not the action routine, so V?FOO and
    V?FOO-WITH can have different preactions.
    
    Also fixed the order in which syntax lines are processed. They're now (again)
    read in definition order so that the earliest preaction will stick, but the
    resulting syntax data is still emitted in reverse order so that the game
    will use later lines first.

---
+++ IntegrationTests/SyntaxTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.Contracts;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class SyntaxTests
+    {
+        private static RoutineAssertionHelper AssertRoutine(string argSpec, string body)
+        {
+            Contract.Requires(argSpec != null);
+            Contract.Requires(!string.IsNullOrWhiteSpace(body));
+
+            return new RoutineAssertionHelper(argSpec, body);
+        }
+
+        [TestMethod]
+        public void First_Preaction_Definition_Per_Action_Name_Should_Persist()
+        {
+            AssertRoutine("", "<TELL " +
+                    "N <=? <GET ,ACTIONS ,V?FOO> ,V-FOO> CR " +
+                    "N <=? <GET ,ACTIONS ,V?FOO-WITH> ,V-FOO> CR " +
+                    "N <=? <GET ,ACTIONS ,V?BAR> ,V-BAR> CR " +
+                    "N <=? <GET ,PREACTIONS ,V?FOO> ,PRE-FOO> CR " +
+                    "N <=? <GET ,PREACTIONS ,V?FOO-WITH> <>> CR " +
+                    "N <=? <GET ,PREACTIONS ,V?BAR> <>> CR " +
+                    ">")
+                .WithGlobal("<ROUTINE V-FOO () <>>")
+                .WithGlobal("<ROUTINE V-BAR () <>>")
+                .WithGlobal("<ROUTINE PRE-FOO () <>>")
+                .WithGlobal("<ROUTINE PRE-FOO-2 () <>>")
+                .WithGlobal("<ROUTINE PRE-BAR () <>>")
+                .WithGlobal("<SYNTAX FOO = V-FOO PRE-FOO>")
+                .WithGlobal("<SYNTAX FOO OBJECT = V-FOO PRE-FOO-2>")
+                .WithGlobal("<SYNTAX FOO OBJECT AT OBJECT = V-FOO>")
+                .WithGlobal("<SYNTAX FOO OBJECT WITH OBJECT = V-FOO <> FOO-WITH>")
+                .WithGlobal("<SYNTAX BAR = V-BAR>")
+                .WithGlobal("<SYNTAX BAR OBJECT = V-BAR PRE-BAR>")
+                .Outputs("1\n1\n1\n1\n1\n1\n");
+        }
+    }
+}
