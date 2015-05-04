736855c060b2dcd00ab873904778d0b59cbfb590
Jesse McGrew <4934394+taradinoc@users.noreply.github.com>
2015-05-03 22:24:03 -0700

    Added DEFAULT-DEFINITION, REPLACE-DEFINITION, DELAY-DEFINITION.

---
+++ ZilfTests/Interpreter/DefinitionsTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zilf;
+
+namespace ZilfTests.Interpreter
+{
+    [TestClass]
+    public class DefinitionsTests
+    {
+        [TestMethod]
+        public void Default_Definition_Should_Be_Used_When_Not_Replaced()
+        {
+            const string CODE = @"
+<DEFAULT-DEFINITION FOO-ROUTINE
+    <DEFINE FOO () 123>>
+
+<FOO>";
+
+            TestHelpers.EvalAndAssert(CODE, new ZilFix(123));
+        }
+
+        [TestMethod]
+        public void Replacement_Should_Be_Used_When_Given()
+        {
+            const string CODE = @"
+<REPLACE-DEFINITION FOO-ROUTINE
+    <DEFINE FOO () 456>>
+
+<DEFAULT-DEFINITION FOO-ROUTINE
+    <DEFINE FOO () 123>>
+
+<FOO>";
+
+            TestHelpers.EvalAndAssert(CODE, new ZilFix(456));
+        }
+
+        [TestMethod]
+        public void Delayed_Replacement_Should_Be_Evaluated_At_The_Right_Place()
+        {
+            const string CODE = @"
+<DELAY-DEFINITION FOO-ROUTINE>
+
+<SETG FOO-RESULT 789>
+
+<REPLACE-DEFINITION FOO-ROUTINE
+    <EVAL <FORM DEFINE FOO '() ,FOO-RESULT>>>
+
+<SETG FOO-RESULT 123>
+
+<DEFAULT-DEFINITION FOO-ROUTINE
+    <EVAL <FORM DEFINE FOO '() ,FOO-RESULT>>>
+
+<FOO>";
+
+            TestHelpers.EvalAndAssert(CODE, new ZilFix(789));
+        }
+    }
+}
