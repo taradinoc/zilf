ea50a93c4b3925ab3f0571fd09088ccb1137d6d8
Jesse McGrew <4934394+taradinoc@users.noreply.github.com>
2015-06-07 06:51:22 -0700

    Fixed exception when using an empty clause with COND or VERSION?.

---
+++ ZilfTests/Interpreter/FlowControlTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zilf;
+
+namespace ZilfTests.Interpreter
+{
+    [TestClass]
+    public class FlowControlTests
+    {
+        [TestMethod]
+        public void COND_Should_Reject_Empty_Clauses()
+        {
+            TestHelpers.EvalAndCatch<InterpreterError>("<COND ()>");
+        }
+
+        [TestMethod]
+        public void VERSION_P_Should_Reject_Empty_Clauses()
+        {
+            TestHelpers.EvalAndCatch<InterpreterError>("<VERSION? ()>");
+        }
+    }
+}
