fea815bc5cc5d5093343622471486b93fbb5260c
Jesse McGrew <4934394+taradinoc@users.noreply.github.com>
2015-11-29 16:49:57 -0800

    Fold constants when compiling arithmetic operations.

---
+++ Zilf.Emit/INumericOperand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zilf.Emit
+{
+    public interface INumericOperand : IOperand
+    {
+        int Value { get; }
+    }
+}
