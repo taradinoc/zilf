553d23f1d4a42f213666f0324f9900c394f52b15
Jesse McGrew <jmcgrew@hansprestige.com>
2016-07-02 13:21:12 -0700

    Fixed infinite loops in ToString and GetHashCode for recursive lists.

---
+++ Zilf/Interpreter/IdentityEqualityComparer.cs
@@ -0,0 +1,42 @@
+﻿/* Copyright 2010, 2016 Jesse McGrew
+ * 
+ * This file is part of ZILF.
+ * 
+ * ZILF is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ZILF is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ZILF.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Zilf.Interpreter
+{
+    class IdentityEqualityComparer<T> : IEqualityComparer<T>
+        where T : class
+    {
+        public static readonly IEqualityComparer<T> Instance = new IdentityEqualityComparer<T>();
+
+        private IdentityEqualityComparer()
+        {
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return ReferenceEquals(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return RuntimeHelpers.GetHashCode(obj);
+        }
+    }
+}
