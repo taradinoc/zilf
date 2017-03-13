10aede3853aa07a4700402bc3c9aa370c0bb8892
Jesse McGrew <jmcgrew@hansprestige.com>
2017-03-13 01:08:05 -0700

    Updated makedist.bat.

--- makedist.bat
+++ makedist.bat
@@ -1,6 +1,6 @@
 @echo off
-if not exist zilf\bin\debug\zilf.exe goto missing
-if not exist zapf\bin\debug\zapf.exe goto missing
+if not exist zilf\bin\release\zilf.exe goto missing
+if not exist zapf\bin\release\zapf.exe goto missing
 
 rd /s /q dist
 mkdir dist
@@ -16,7 +16,7 @@ copy zilf\bin\release\Antlr3.Runtime.dll dist\bin
 copy distfiles\AntlrLicense.txt dist\bin
 copy zilf\bin\release\zilf.exe dist\bin
 copy zilf\bin\release\zilf.emit.dll dist\bin
-copy zilf\bin\release\zilf.stringencoding.dll dist\bin
+copy zilf\bin\release\zilf.common.dll dist\bin
 copy COPYING.txt dist\bin
 copy zapf\bin\release\zapf.exe dist\bin
 
@@ -25,13 +25,14 @@ copy zapf_manual.html dist\doc
 copy zilf\quickref.txt dist\doc
 
 copy library\*.zil dist\library
+copy library\*.mud dist\library
 copy library\LICENSE.txt dist\library
 copy library\ZIL_ZILF_differences.txt dist\library
 
 copy examples\*.zil dist\sample
-
 copy examples\advent\*.zil dist\sample\advent
-copy examples\advent\*.txt dist\sample\advent
+
+del /s dist\*~
 
 exit /b 0
 
