90cc1a55d437afaa505b495e8cc6f53bb679aecc
Jesse McGrew <4934394+taradinoc@users.noreply.github.com>
2018-04-22 16:14:32 -0700

    Use standard verb names in test.ps1.

--- Library/tests/test.ps1
+++ Library/tests/test.ps1
@@ -3,7 +3,7 @@ $zapfPath = "..\..\Zapf\bin\Debug\zapf.exe"
 $czlrPath = ".\ConsoleZLR.exe"
 $includeDir = ".."
 
-function Compile-Zil {
+function Invoke-Zilf {
     param ([string]$SrcFile = $(throw "SrcFile parameter is required."))
     $output = (& $zilfPath -ip $includeDir $SrcFile 2>&1)
     if ($LASTEXITCODE -eq 0) {
@@ -14,7 +14,7 @@ function Compile-Zil {
     }
 }
 
-function Assemble-Zap {
+function Invoke-Zapf {
     param ([string]$SrcFile = $(throw "SrcFile parameter is required."))
     $output = (& $zapfPath $SrcFile 2>&1)
     if ($LASTEXITCODE -eq 0) {
@@ -25,25 +25,25 @@ function Assemble-Zap {
     }
 }
 
-function Run-Zcode {
+function Invoke-ZLR {
     param ([string]$StoryFile = $(throw "StoryFile parameter is required."))
     & $czlrPath -nowait -dumb $StoryFile
 }
 
-function Run-Test {
+function Test-Scenario {
     param ($TestName = $(throw "TestName parameter is required."),
            [switch]$Silent = $false)
-    
+
     $testFile = ".\test-" + $TestName + ".zil"
-    
+
     Write-Progress -Activity "Testing $TestName" -Status "Compiling $testFile" -Id 1
-    if (Compile-Zil $testFile) {
+    if (Invoke-Zilf $testFile) {
         $zapFile = [io.path]::ChangeExtension($testFile, ".zap")
         Write-Progress -Activity "Testing $TestName" -Status "Assembling $zapFile" -Id 1
-        if (Assemble-Zap $zapFile) {
+        if (Invoke-Zapf $zapFile) {
             $storyFile = [io.path]::ChangeExtension($zapFile, ".z3")
             Write-Progress -Activity "Testing $testFile" -Status "Executing $storyFile" -Id 1
-            $output = $(Run-Zcode $storyFile)
+            $output = $(Invoke-ZLR $storyFile)
             if ($output -match "^PASS$") {
                 Write-Progress -Activity "Testing $testFile" -Status "OK"
                 return $true
@@ -59,23 +59,25 @@ function Run-Test {
     return $false
 }
 
-Set-Alias test Run-Test
+Set-Alias test Test-Scenario
 
-function Get-TestNames {
-    dir test-*.zil | foreach { $_.Name -replace '^test-(.*)\.zil$', '$1' }
+function Get-ScenarioNames {
+    Get-ChildItem test-*.zil | ForEach-Object { $_.Name -replace '^test-(.*)\.zil$', '$1' }
 }
 
-function Test-All {
-    $testNames = Get-TestNames
+function Test-Scenarios {
+    $testNames = Get-ScenarioNames
     $completed = 0
-    
+
     foreach ($t in $testNames) {
         Write-Progress -Activity "Running all tests" -Status $t -PercentComplete (($completed) * 100 / $testNames.Count)
-    
-        if (Run-Test $t -Silent) {$status = "OK"} else {$status = "Fail"}
+
+        if (Test-Scenario $t -Silent) {$status = "OK"} else {$status = "Fail"}
         $completed++
-        
+
         $hash = @{Name=$t; Status=$status}
         New-Object PSObject -Property $hash
     }
 }
+
+Set-Alias testall Test-Scenarios
