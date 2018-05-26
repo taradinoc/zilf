ec417609d6073d4f14d14773891c51f39765d9c0
Jesse McGrew <4934394+taradinoc@users.noreply.github.com>
2018-05-26 10:35:46 -0700

    Update various projects to new .csproj format.

--- Zapf/Properties/AssemblyInfo.cs
+++ Zapf/Properties/AssemblyInfo.cs
@@ -17,46 +17,7 @@
  */
 
 using System;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ZAPF")]
-[assembly: AssemblyDescription("Z-code Assembler Program of the Future")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Jesse McGrew")]
-[assembly: AssemblyProduct("ZAPF")]
-[assembly: AssemblyCopyright("Copyright © Jesse McGrew 2009-2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("844647a3-aa5f-464f-8caa-6c2dc2bd0750")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.*")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Zapf.Tests")]
