9537c1c82db93ecf3840ae78b80fba68d9476e79
Tara McGrew <jmcgrew@hansprestige.com>
2025-10-22 23:37:39 -0700

    Reverted an accidentally committed failing test

--- test/Zilf.Common.Tests/SuffixTreeTests.cs
+++ test/Zilf.Common.Tests/SuffixTreeTests.cs
@@ -231,18 +231,5 @@ namespace Zilf.Common.Tests
 
             Assert.AreEqual(3, ssc.CountOccurrences("here"));
         }
-
-        [TestMethod]
-        public void TestSearchableStringCollection_Overlapping()
-        {
-            const string Substring = "aaaaaaaaaa"; // 10 a's
-
-            var ssc = new IndexedStringCollection
-            {
-                "aaaaaaaaaaaaaaaaaaaa"  // 20 a's
-            };
-
-            Assert.AreEqual(2, ssc.CountOccurrences(Substring));
-        }
     }
 }
